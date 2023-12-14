8b3066c8430707ae85fe399b917e824b052fe4fd
= <=>
2023-12-14 21:17:27 +0530

    feat: added error state

--- src/handlers/getAnswerHandler.ts
+++ src/handlers/getAnswerHandler.ts
@@ -16,26 +16,34 @@ const client = new DiscussServiceClient({
 });
 
 export const getAnswerHandler = async (req: Request, res: Response) => {
-    console.log('Inside get answer handler')
-    const messages = req.body.messages as Message[]
-    const operationType = req.body['operation-type']
-    console.log('Operation type: ' + operationType)
-    const prompt = getPrompt(operationType)
-    console.log(prompt)
-    const result = await client.generateMessage({
-      model: MODEL_NAME, // Required. The model to use to generate the result.
-      temperature: 0, // Optional. Value `0.0` always uses the highest-probability result.
-      candidateCount: 1, // Optional. The number of candidate results to generate.
-      prompt: {
-        context: prompt,
-        messages,
-      },
-    });
-    console.log(result[0])
-    if (result[0].candidates) {
-        return res.status(200).json({
+    try {
+        console.log('Inside get answer handler')
+        const messages = req.body.messages as Message[]
+        const operationType = req.body['operation-type']
+        console.log('Operation type: ' + operationType)
+        const prompt = getPrompt(operationType)
+        console.log(prompt)
+        const result = await client.generateMessage({
+          model: MODEL_NAME, // Required. The model to use to generate the result.
+          temperature: 0, // Optional. Value `0.0` always uses the highest-probability result.
+          candidateCount: 1, // Optional. The number of candidate results to generate.
+          prompt: {
+            context: prompt,
+            messages,
+          },
+        });
+        console.log(result[0])
+        if (result[0].candidates) {
+            return res.status(200).json({
+                type: 'assistant',
+                content: result[0].candidates[0].content
+            })
+        }
+    } catch(e) {
+        console.log(e)
+        res.status(200).json({
             type: 'assistant',
-            content: result[0].candidates[0].content
+            content: 'Sorry for the inconvenience, something went wrong'
         })
     }
 }
